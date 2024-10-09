2f38596e9d712c46f3a6de1fad68dd2497b76346
Sunil Kumar <64345574+SunilKumar2501@users.noreply.github.com>
2024-10-09 23:20:44 +0530

    Minor changes

--- app.py
+++ app.py
@@ -4,52 +4,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.set_page_config(layout = "wide",page_title="Indian Startup Analysis")
+st.set_page_config(layout="wide", page_title="Indian Startup Analysis")
 st.title("Data Analysis of Indian Startup Dataset")
 # some data preprocessing
 df = pd.read_csv("startup_clean.csv")
 df["investors"] = df["investors"].fillna("Undisclosed")
 lst = sorted(set(df["investors"].str.split(",").sum()))
 del lst[0:2]
-df["date"] = pd.to_datetime(df["date"],errors = "coerce")
+df["date"] = pd.to_datetime(df["date"], errors="coerce")
 df["year"] = df["date"].dt.year
 df["month"] = df["date"].dt.month
-df.replace({
-    "Bangalore" : "Bengaluru"
-},inplace = True)
-df.replace({
-    "Flipkart.com": "Flipkart"
-},inplace = True)
-df["startup"].replace({'"BYJU\\\\\'S"': "Byju's"}, inplace=True)
-df.replace({"https://www.wealthbucket.in/" : "WealthBucket"},inplace = True)
+df.replace({"Bangalore": "Bengaluru"}, inplace=True)
+df.replace({"Flipkart.com": "Flipkart"}, inplace=True)
+df ["startup"] = df["startup"].replace({'"BYJU\\\\\'S"': "BYJU'S"})
+df.replace({"https://www.wealthbucket.in/": "WealthBucket"}, inplace=True)
+
 
 def load_overall_analysis():
-    #total invested amount
+    # total invested amount
     total = df["amount"].sum()
-    #maximum amount infused in single startup
-    max = df.groupby("startup")["amount"].max().sort_values(ascending=False).head(2).values[0]
-    #avergae invested amount
+    # maximum amount infused in single startup
+    max = (
+        df.groupby("startup")["amount"]
+        .max()
+        .sort_values(ascending=False)
+        .head(2)
+        .values[0]
+    )
+    # avergae invested amount
     mean = df.groupby("startup")["amount"].sum().mean()
-    #total funded startups
+    # total funded startups
     all = df["startup"].nunique()
 
-    col1,col2,col3,col4 = st.columns(4)
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
-        st.metric("Total" , str(total)+"Cr")
+        st.metric("Total", str(total) + "Cr")
     with col2:
         st.metric("Max", str(max) + "Cr")
     with col3:
-        st.metric("Average",str(round(mean,2))+ "Cr")
+        st.metric("Average", str(round(mean, 2)) + "Cr")
     with col4:
-        st.metric("Funded Startup",str(all))
+        st.metric("Funded Startup", str(all))
     col1, col2 = st.columns(2)
     st.divider()
     with col1:
         st.header("MoM graph")
-        selected_option = st.selectbox("Select Type", ["Total", "Count","All Total","All Count"])
+        selected_option = st.selectbox(
+            "Select Type", ["Total", "Count", "All Total", "All Count"]
+        )
         if selected_option == "Total":
             temp_df = df.groupby(["year", "month"], as_index=False)["amount"].sum()
-            temp_df["x_axis"] = temp_df["month"].astype("str") + "/" + (temp_df["year"] % 100).astype('str')
+            temp_df["x_axis"] = (
+                temp_df["month"].astype("str")
+                + "/"
+                + (temp_df["year"] % 100).astype("str")
+            )
 
             fig3, ax3 = plt.subplots()
             # Create a dropdown to select a year
@@ -61,7 +70,11 @@ def load_overall_analysis():
             st.pyplot(fig3)
         elif selected_option == "Count":
             temp_df = df.groupby(["year", "month"], as_index=False)["amount"].count()
-            temp_df["x_axis"] = temp_df["month"].astype("str") + "/" + (temp_df["year"] % 100).astype('str')
+            temp_df["x_axis"] = (
+                temp_df["month"].astype("str")
+                + "/"
+                + (temp_df["year"] % 100).astype("str")
+            )
 
             fig3, ax3 = plt.subplots()
             # Create a dropdown to select a year
@@ -73,61 +86,110 @@ def load_overall_analysis():
             st.pyplot(fig3)
         elif selected_option == "All Total":
             temp_df = df.groupby(["year", "month"], as_index=False)["amount"].sum()
-            temp_df["x_axis"] = temp_df["month"].astype("str") + "/" + (temp_df["year"] % 100).astype('str')
+            temp_df["x_axis"] = (
+                temp_df["month"].astype("str")
+                + "/"
+                + (temp_df["year"] % 100).astype("str")
+            )
             fig3, ax3 = plt.subplots()
             ax3.plot(temp_df["x_axis"], temp_df["amount"])
             st.pyplot(fig3)
         else:
             temp_df = df.groupby(["year", "month"], as_index=False)["amount"].count()
-            temp_df["x_axis"] = temp_df["month"].astype("str") + "/" + (temp_df["year"] % 100).astype('str')
+            temp_df["x_axis"] = (
+                temp_df["month"].astype("str")
+                + "/"
+                + (temp_df["year"] % 100).astype("str")
+            )
             fig3, ax3 = plt.subplots()
             ax3.plot(temp_df["x_axis"], temp_df["amount"])
             st.pyplot(fig3)
-    col1,col2= st.columns(2)
+    col1, col2 = st.columns(2)
     with col1:
         st.header("Sector wise Investment graph")
         selected_option = st.selectbox("Select Type", ["Total", "Count"])
         if selected_option == "Total":
-            temp_df = df.groupby("vertical")["amount"].sum().sort_values(ascending=False).head(10)
+            temp_df = (
+                df.groupby("vertical")["amount"]
+                .sum()
+                .sort_values(ascending=False)
+                .head(10)
+            )
             fig3, ax3 = plt.subplots()
             ax3.pie(temp_df, labels=temp_df.index, autopct="%0.01f%%")
             st.pyplot(fig3)
         else:
-            temp_df = df.groupby("vertical")["investors"].count().sort_values(ascending=False).head(10)
+            temp_df = (
+                df.groupby("vertical")["investors"]
+                .count()
+                .sort_values(ascending=False)
+                .head(10)
+            )
             fig3, ax3 = plt.subplots()
             ax3.pie(temp_df, labels=temp_df.index, autopct="%0.01f%%")
             st.pyplot(fig3)
 
-    col1,col2,col3,col4= st.columns(4)
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
         st.header("Type of Funding")
-        rounds_funding = df.groupby("round")["amount"].sum().sort_values(ascending=False).reset_index()
+        rounds_funding = (
+            df.groupby("round")["amount"]
+            .sum()
+            .sort_values(ascending=False)
+            .reset_index()
+        )
         rounds_funding = rounds_funding.set_index("round")
         st.dataframe(rounds_funding)
     with col2:
         st.header("City Wise Funding")
-        city_funding = df.groupby("city")["amount"].sum().sort_values(ascending = False).reset_index().set_index("city").head(50)
+        city_funding = (
+            df.groupby("city")["amount"]
+            .sum()
+            .sort_values(ascending=False)
+            .reset_index()
+            .set_index("city")
+            .head(50)
+        )
         st.dataframe(city_funding)
     with col3:
         st.header("Top Startup Year Wise ")
-        new = df.groupby(["year", "startup"], as_index=False)["amount"].sum().sort_values(["year", "amount"],ascending=[True,False]).drop_duplicates("year", keep="first")
+        new = (
+            df.groupby(["year", "startup"], as_index=False)["amount"]
+            .sum()
+            .sort_values(["year", "amount"], ascending=[True, False])
+            .drop_duplicates("year", keep="first")
+        )
         new["year"] = new["year"].astype("str")
         new = new.set_index("year")
         st.dataframe(new)
     with col4:
         st.header("Top Startup Overall ")
-        data = df.groupby("startup", as_index=False)["amount"].sum().sort_values("amount", ascending=False)[["startup", "amount"]]
+        data = (
+            df.groupby("startup", as_index=False)["amount"]
+            .sum()
+            .sort_values("amount", ascending=False)[["startup", "amount"]]
+        )
         data = data.set_index("startup")
         st.dataframe(data.head(100))
 
     st.header("Top Investors Overall ")
-    data = df.groupby("investors")["amount"].sum().sort_values(ascending = False).head(100)
+    data = (
+        df.groupby("investors")["amount"].sum().sort_values(ascending=False).head(100)
+    )
     st.dataframe(data)
 
     st.header("Funding Heatmap")
-    grouped_data = df.groupby("startup", )["amount"].sum().head(500)
+    grouped_data = (
+        df.groupby(
+            "startup",
+        )["amount"]
+        .sum()
+        .head(500)
+    )
     fig, ax = plt.subplots(figsize=(20, 10))  # Adjust figure size as needed
-    sns.heatmap(grouped_data.to_frame().T, ax=ax, cmap="YlGnBu")  # Use your preferred colormap
+    sns.heatmap(
+        grouped_data.to_frame().T, ax=ax, cmap="YlGnBu"
+    )  # Use your preferred colormap
     plt.title("Total Funding Amount per Startup")
     plt.tight_layout()
 
@@ -135,19 +197,28 @@ def load_overall_analysis():
     st.pyplot(fig)
 
 
-
 def load_investor_detail(investor):
     st.title(investor)
     # load last 5 investments of the investor
-    last5df = df[df["investors"].str.contains(investor)].head()[["date","startup", "vertical", "city", "round", "amount"]].set_index("date")
+    last5df = (
+        df[df["investors"].str.contains(investor)]
+        .head()[["date", "startup", "vertical", "city", "round", "amount"]]
+        .set_index("date")
+    )
     st.subheader("Most Recent Investments")
     st.dataframe(last5df)
 
     try:
-    #biggest investments
-        col1,col2 = st.columns(2)
+        # biggest investments
+        col1, col2 = st.columns(2)
         with col1:
-            biggest_inv = df[df["investors"].str.contains(investor)].groupby("startup")["amount"].sum().sort_values(ascending=False).head(5)
+            biggest_inv = (
+                df[df["investors"].str.contains(investor)]
+                .groupby("startup")["amount"]
+                .sum()
+                .sort_values(ascending=False)
+                .head(5)
+            )
             st.subheader("Biggest Investments")
             st.dataframe(biggest_inv)
             st.write("Bar Graph Representation")
@@ -155,7 +226,11 @@ def load_investor_detail(investor):
             ax.bar(biggest_inv.index, biggest_inv.values)
             st.pyplot(fig)
         with col2:
-            vertical_series = df[df["investors"].str.contains(investor)].groupby("vertical")["amount"].sum()
+            vertical_series = (
+                df[df["investors"].str.contains(investor)]
+                .groupby("vertical")["amount"]
+                .sum()
+            )
 
             if vertical_series.empty:
                 st.warning(f"No data found for investor: {investor}")
@@ -170,14 +245,22 @@ def load_investor_detail(investor):
 
         col3, col4 = st.columns(2)
         with col3:
-            rounds_series = df[df["investors"].str.contains(investor)].groupby("round")["amount"].sum()
+            rounds_series = (
+                df[df["investors"].str.contains(investor)]
+                .groupby("round")["amount"]
+                .sum()
+            )
             st.subheader("Rounds Stage ")
             fig, ax = plt.subplots()
             ax.pie(rounds_series, labels=rounds_series.index, autopct="%0.01f%%")
             st.pyplot(fig)
 
         with col4:
-            city_series = df[df["investors"].str.contains(investor)].groupby("city")["amount"].sum()
+            city_series = (
+                df[df["investors"].str.contains(investor)]
+                .groupby("city")["amount"]
+                .sum()
+            )
             st.subheader("Most Invested City")
             fig, ax = plt.subplots()
             ax.pie(city_series, labels=city_series.index, autopct="%0.01f%%")
@@ -185,7 +268,11 @@ def load_investor_detail(investor):
 
         col5, col6 = st.columns(2)
         with col5:
-            year_series = df[df["investors"].str.contains(investor)].groupby("year")["amount"].sum()
+            year_series = (
+                df[df["investors"].str.contains(investor)]
+                .groupby("year")["amount"]
+                .sum()
+            )
             st.subheader("Year on Year Investments")
             val = year_series.index.astype("str")
             fig, ax = plt.subplots()
@@ -195,15 +282,19 @@ def load_investor_detail(investor):
             st.subheader("Similar Investors with regards to Sectors ")
             investor = df[df["investors"].str.contains(investor)]
             # Check for overlapping verticals between investor and df DataFrames
-            matching_verticals = np.intersect1d((investor["vertical"]), (df["vertical"].unique()))
+            matching_verticals = np.intersect1d(
+                (investor["vertical"]), (df["vertical"].unique())
+            )
             v = list(matching_verticals)
             # Filter based on matching verticals
-            filtered_df = df[df["vertical"].isin(v)][["startup", "vertical", 'investors']].set_index("vertical")
+            filtered_df = df[df["vertical"].isin(v)][
+                ["startup", "vertical", "investors"]
+            ].set_index("vertical")
             st.dataframe(filtered_df)
 
     except:
         st.error(f"Data is not enough for further analysis")
-        #sector invested
+        # sector invested
         # vertical_series = df[df["investors"].str.contains(investor)].groupby("vertical")["amount"].sum()
         # st.subheader("Biggest Sectors Invested")
         # st.dataframe(vertical_series.head(5))
@@ -215,7 +306,7 @@ def load_investor_detail(investor):
 
 def company_details(name):
     st.title(name)
-    col1,col2,col3 = st.columns(3)
+    col1, col2, col3 = st.columns(3)
     # Industry
     with col1:
         industry = df[df["startup"] == name]["vertical"].drop_duplicates()
@@ -233,7 +324,9 @@ def company_details(name):
     st.subheader("Funding Rounds")
     temp_df = df.copy()
     temp_df["date"] = temp_df["date"].astype("str")
-    details = temp_df[temp_df["startup"]== name][["round","investors","date","amount"]].set_index("round")
+    details = temp_df[temp_df["startup"] == name][
+        ["round", "investors", "date", "amount"]
+    ].set_index("round")
     st.dataframe(details)
 
     st.subheader("Similar Company")
@@ -242,21 +335,25 @@ def company_details(name):
     s = df[df["startup"] == name]
     v = s["subVertical"].dropna().unique()
     v_list = list(v)
-    filtered_df = temp_df[temp_df["subVertical"].isin(v_list)][["startup","subVertical","date"]].set_index("startup")
+    filtered_df = temp_df[temp_df["subVertical"].isin(v_list)][
+        ["startup", "subVertical", "date"]
+    ].set_index("startup")
 
     st.dataframe(filtered_df)
 
 
 st.sidebar.title("Startup Funding Analysis")
 
-option = st.sidebar.selectbox("Select One",["Overall Analysis","Startup","Investor"])
+option = st.sidebar.selectbox("Select One", ["Overall Analysis", "Startup", "Investor"])
 
 if option == "Overall Analysis":
     # st.title("Overall Analysis")
     btn0 = st.sidebar.button("Show Overall analysis")
     load_overall_analysis()
 elif option == "Startup":
-    selected = st.sidebar.selectbox("Select Startup",sorted(df["startup"].unique().tolist()))
+    selected = st.sidebar.selectbox(
+        "Select Startup", sorted(df["startup"].unique().tolist())
+    )
     button1 = st.sidebar.button("Find Startup Details")
     # st.title("Startup Analysis")
     print(button1)
@@ -269,5 +366,3 @@ else:
     if button2:
         st.divider()
         load_investor_detail(selected)
-
-
